339369482de7915df903c822b0f633c0e5eb24ba
Raynald de Lahondes <raynald@delahondes.com>
2025-08-21 23:48:04 +0200

    typo in hellogoodbye workflow (Param has no float yet)

--- examples/hello_goodbye_with_failures.py
+++ examples/hello_goodbye_with_failures.py
@@ -5,7 +5,7 @@ LOREM_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do e
 class Params(metaclass=ParamSpec):
     name = Param.string(required=True, help="State your name here")
     how_many = Param.integer(default=1, help="How many times to say hello")
-    failure_rate = Param. float(default=10, help="% of failure for the steps")
+    failure_rate = Param.integer(default=10, help="% of failure for the steps")
 
 def hellogoodbye(params: Params):
 
