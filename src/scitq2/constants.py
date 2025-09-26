afc4e6234f9f82981b0f93bef1ff1c20c98f71ef
Raynald de Lahondes <raynald@delahondes.com>
2025-09-26 10:50:59 +0200

    Switching default task status from O (on hold) to I (inactive)

--- src/scitq2/constants.py
+++ src/scitq2/constants.py
@@ -1,3 +1,3 @@
-DEFAULT_TASK_STATUS = "O" # tasks are on hold by default
+DEFAULT_TASK_STATUS = "I" # tasks are inactive by default
 ACTIONS = ["untar","gunzip"]
 DEFAULT_RECRUITER_TIMEOUT = 30 # how many seconds do we wait before recruiting
